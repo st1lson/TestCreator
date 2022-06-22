1de0cf23f3f6a58de497fa12b0b17ef45ae7d643
Max <rendon4ik@gmail.com>
2022-06-22 16:23:34 +0300

    Add test controller and auth policy

---
+++ Backend/TestCreator.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCreator.Data.Models;
+using TestCreator.Data.Repositories;
+
+namespace TestCreator.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TestController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+
+        public TestController(UnitOfWork unitOfWork, UserManager<User> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "Auth")]
+        public IActionResult Get([FromServices] IHttpContextAccessor accessor)
+        {
+            string userId = accessor.HttpContext!.User.Claims.First().Value;
+            IEnumerable<Test> tests = _unitOfWork.TestRepository.Get(t => t.UserId == userId);
+            return Ok(tests);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Policy = "Auth")]
+        public IActionResult GetById([FromServices] IHttpContextAccessor accessor, string id)
+        {
+            string userId = accessor.HttpContext!.User.Claims.First().Value;
+            Test test = _unitOfWork.TestRepository.GetById(id);
+            if (test.UserId != userId)
+            {
+                return BadRequest();
+            }
+
+            return Ok(test);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "Auth")]
+        public async Task<IActionResult> Create(List<Question> questions)
+        {
+            return Ok();
+        }
+    }
+}
--- Backend/TestCreator.WebAPI/Startup.cs
+++ Backend/TestCreator.WebAPI/Startup.cs
@@ -1,16 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using TestCreator.Data.DbContexts;
+using TestCreator.Data.Models;
 
 namespace TestCreator.WebAPI
 {
@@ -31,6 +31,42 @@ namespace TestCreator.WebAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestCreator.WebAPI", Version = "v1" });
+            });
+
+            services
+                .AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 1;
+                })
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(config =>
+                {
+                    config.RequireHttpsMetadata = true;
+                    config.SaveToken = true;
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("qwerty")),
+                        ValidateIssuerSigningKey = true,
+                        ValidateAudience = false,
+                        ValidateIssuer = false
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Auth", policy => policy.RequireClaim(JwtRegisteredClaimNames.Typ, "Auth"));
             });
         }
 
